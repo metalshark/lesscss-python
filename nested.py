42bc2dddbfe89624754f36fe41b6ccd1052e8dcd
Beech Horn <beechhorn@gmail.com>
2010-07-05 19:09:01 +0000

    Fixed bug with unterminated blocks

--- nested.py
+++ nested.py
@@ -10,6 +10,9 @@ def parse_nested(less):
     while depth:
         pos = less.find('}') + 1
         
+        if depth and not pos:
+            raise ValueError()
+        
         chunk = less[:pos]
         less = less[pos:]
         
