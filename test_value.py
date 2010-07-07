9ec1ae0b422f483b68419a9446ab2ab7bbe470a2
Beech Horn <beechhorn@gmail.com>
2010-07-07 15:03:37 +0000

    Support for adding colours and tracking down the final value of a constant

--- test_value.py
+++ test_value.py
@@ -3,15 +3,51 @@
 
 
 import unittest
-from lessc import compile
+from value import get_colour, get_value, parse_value
 
 
 class TestAddition(unittest.TestCase):
-    pass
+    def test_colour(self):
+        self.assertEqual(get_value('#111 + #111', {}), '#222')
+        
+    def test_example(self):
+        self.assertEqual(get_value('#5B83AD + #111', {}), '#6c94be')
+
+
+class TestColour(unittest.TestCase):
+    def test_blue_lower(self):
+        self.assertEqual(get_colour('#0000ff'), 'blue')
+        
+    def test_blue_mixed(self):
+        self.assertEqual(get_colour('#0000Ff'), 'blue')
+        
+    def test_blue_upper(self):
+        self.assertEqual(get_colour('#0000FF'), 'blue')
+        
+    def test_triple_lower(self):
+        self.assertEqual(get_colour('#aaaaaa'), '#aaa')
+        
+    def test_triple_mixed(self):
+        self.assertEqual(get_colour('#aAaAaA'), '#aaa')
+        
+    def test_triple_number(self):
+        self.assertEqual(get_colour('#777777'), '#777')
+        
+    def test_triple_upper(self):
+        self.assertEqual(get_colour('#AAAAAA'), '#aaa')
+    
+    
+class TestParse(unittest.TestCase):
+    def setUp(self):
+        self.constants = {'@nice-blue': 'blue'}
+
+    def test_constant(self):
+        self.assertEqual(parse_value('@nice-blue', self.constants),
+                         [{'type': 'colour', 'value': '#0000ff'}])
 
 
 def suite():
-    test_cases = (TestAddition,)
+    test_cases = (TestAddition, TestColour, TestParse)
     
     suite = unittest.TestSuite()
     
